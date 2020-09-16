e67e0aba856992d381ef1e6fd2349a4b17627c88
AdamStammerWSU <adam.stammer@go.winona.edu>
2020-09-16 12:11:44 -0500

    partA working

--- Simulation/src/edu/wsu/os/Main.java
+++ Simulation/src/edu/wsu/os/Main.java
@@ -1,8 +1,15 @@
 package edu.wsu.os;
 
+import java.io.FileNotFoundException;
+
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.println("Howdy!");
+	public static void main(String[] args) throws FileNotFoundException {
+
+		DataManager dm = new DataManager();
+		
+		dm.retrieveFile();
+		dm.performTasks();
+		dm.finalTime();
 	}
 }
